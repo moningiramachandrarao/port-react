5538a28c0db524653646ada4fd051843e1b8323a
Ramachandra Rao Moningi <moningiramachandrarao@gmail.com>
2023-10-19 15:05:43 +0530

    commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,12 +3,12 @@ function Home(){
     return(
         <div>
         <div class="container bg-warning">
-            <nav class="navbar">
+            <nav class="navbar ">
              <div class="navbar-brand"><Link to="/home1" class="nav-link bg-dark text-light mx-2 p-2">Portfolio</Link></div>
              <div class="nav">
-                <Link to="/home1" class="nav-link bg-dark mx-2 text-light">Home</Link>
-                <Link to="/education" class="nav-link bg-dark mx-2 text-light">Education</Link>
-                <Link to="/contact" class="nav-link bg-dark mx-2 text-light">Contact</Link>
+                <Link to="/home1" class="nav-link bg-dark mx-2 text-light p-2">Home</Link>
+                <Link to="/education" class="nav-link bg-dark mx-2 text-light p-2">Education</Link>
+                <Link to="/contact" class="nav-link bg-dark mx-2 text-light p-2">Contact</Link>
              </div>
             </nav>
         </div>
