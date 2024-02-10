6c3295547d555b9117de886e8cdae4a484e806d3
Ramachandra Rao Moningi <moningiramachandrarao@gmail.com>
2024-02-10 17:46:25 +0530

    changed

--- src/components/Home1.js
+++ src/components/Home1.js
@@ -1,20 +1,20 @@
 import React from "react";
 import Typewriter from "typewriter-effect";
-import Brahmaji from "./Brahmaji.jpeg";
+import Ramachandra from "./rc.jpeg";
 
 function Home1() {
   return (
     <div>
       <div className="my-3 d-flex">
-        <img src={Brahmaji} alt="Rc" style={{ maxHeight: "90vh", width: "30%" }} />
+        <img src={Ramachandra} alt="Rc" style={{ maxHeight: "90vh", width: "30%" }} />
         <div className="text-center text-light" style={{ marginTop: "10%", marginLeft: "15%" }}>
           <h1>HAI FRIENDS!</h1>
-          <h1 class="text-danger">I'M Brahmaji
+          <h1 class="text-danger">I'M Ramachandra Rao
         </h1>
         <h3 class="text-danger mt-5">
         <Typewriter
             options={{
-              strings: ["Web Developer","AI","Competative Coder"],
+              strings: ["Web Developer","Data Analyst","Competative Coder"],
               autoStart: true,
               loop: true,
             }}
